d72205d72b5ccf994bb5b7bd73323d21b915b21c
radiantly <buildingsomethingfun@gmail.com>
2025-07-10 09:40:57 +0530

    Use gh-pages

--- run.bat
+++ run.bat
@@ -7,8 +7,10 @@ exit /B
 git config user.name "Ad Bot"
 git config user.email bot@royalroadads.com
 git fetch origin
-git reset --hard origin/main
+git switch gh-pages
+git reset --hard origin/gh-pages
+git rebase origin/main
 python main.py
 git add docs\300x250
 git commit -m "Automated: Add ads"
-git push origin main
+git push --force-with-lease origin gh-pages
